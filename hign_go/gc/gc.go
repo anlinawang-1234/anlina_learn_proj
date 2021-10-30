881327fc18a0d1acfdff2617caf08ca1cec78d05
anlinawang <anlinawang@tencent.com>
2021-10-30 18:19:43 +0800

    ci

--- hign_go/gc/gc.go
+++ hign_go/gc/gc.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
+	"runtime/trace"
 	"time"
 
 )
 
 func main(){
+	//gc1()
+	//gcTrace()
+	traceOut()
+}
+
+// 生成trace 图 使用 go tool trace trace.out
+func traceOut(){
+	f, _ := os.Create("trace.out")
+	defer f.Close()
+	trace.Start(f)
+	defer trace.Stop()
+	keepAlloc()
+}
+
+var cache = map[interface{}]interface{}{}
+func keepAlloc(){
+	for i := 0; i < 10000; i++{
+		cache[i] = make([]byte, 1<<10)
+	}
+}
+
+func gcTrace(){
+	// 查看gc信息
+	for i := 0; i < 10000; i++{
+		_ = make([]byte, 1<<20)
+	}
+}
+func gc1() {
 	startTime := time.Now()
 	i := 0
 	go func() {
--- hign_go/reflect/reflect.go
+++ hign_go/reflect/reflect.go
@@ -1,1 +1,31 @@
-package reflect
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Execute struct {
+}
+
+func main(){
+	myExec := Execute{}
+	execType := reflect.TypeOf(&myExec)
+	for i := 0; i < execType.NumMethod(); i++{
+		fmt.Println("method", i+1, execType.Method(i))
+		reflect.ValueOf(&myExec).MethodByName(execType.Method(i).Name).Call(nil)
+	}
+	reflect.ValueOf(&myExec).MethodByName("Func1").Call(nil)
+	fmt.Println("reflect.ValueOf(&myExec)", reflect.ValueOf(&myExec))
+	fmt.Println("reflect.TypeOf(&myExec)", reflect.TypeOf(&myExec))
+}
+
+func(this *Execute)Func1(){
+	fmt.Println("func1")
+}
+func (this *Execute)Func2(){
+	fmt.Println("func2")
+}
+func(this *Execute)Func3(){
+	fmt.Println("func3")
+}
