7e51ae86ae268c8a2f4c972c57b375e11c897c19
anlinawang <anlinawang@tencent.com>
2021-10-30 18:18:18 +0800

    ci

--- hign_go/sync_pool/sync_pool.go
+++ hign_go/sync_pool/sync_pool.go
@@ -1,5 +1,27 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"net/http"
+)
 
+func newBuf()[]byte{
+	return make([]byte, 10<<20)
 }
+
+func main(){
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+
+	http.HandleFunc("/example", func(writer http.ResponseWriter, request *http.Request) {
+		b := newBuf()
+
+		for idx := range b{
+			b[idx] = 1
+		}
+		fmt.Fprintf(writer, "done +%v", request.URL.Path)
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
